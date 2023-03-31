9b2497fbc56ddade6040ec88eb40805ef8968774
Zein Tonconi <zeintonconi@gmail.com>
2023-03-30 21:19:06 -0400

    Todo Category Component Created

--- src/app/todo-app/todo-app.module.ts
+++ src/app/todo-app/todo-app.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TodoAppRoutingModule } from './todo-app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CategoryComponent
   ],
   imports: [
     CommonModule,
