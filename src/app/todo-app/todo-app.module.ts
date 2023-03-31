9e76cd085b972e7d9a67c7662ae191b56f8326ff
Zein Tonconi <zeintonconi@gmail.com>
2023-03-30 21:11:36 -0400

    Todo App Module Created

---
+++ src/app/todo-app/todo-app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoAppRoutingModule } from './todo-app-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TodoAppRoutingModule
+  ]
+})
+export class TodoAppModule { }
