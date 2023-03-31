aa3d445ccd5b3dcd73a9e76a3d38d11398eacfa0
Zein Tonconi <zeintonconi@gmail.com>
2023-03-30 21:20:16 -0400

    Todo Configuration Component Created

--- src/app/todo-app/todo-app.module.ts
+++ src/app/todo-app/todo-app.module.ts
@@ -5,13 +5,15 @@ import { TodoAppRoutingModule } from './todo-app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CategoryComponent } from './category/category.component';
 import { TagComponent } from './tag/tag.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     CategoryComponent,
-    TagComponent
+    TagComponent,
+    ConfigurationComponent
   ],
   imports: [
     CommonModule,
