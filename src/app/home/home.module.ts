0984274a974ef2a86253576e30d51d2cb4edd6e7
Zein Tonconi <zeintonconi@gmail.com>
2023-03-30 21:25:07 -0400

    Changing the name of Home Component in Home Module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    LandingPageComponent
   ],
   imports: [
     CommonModule,
