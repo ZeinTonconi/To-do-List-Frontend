7605be7243241ea9896c5c70bff4774213d6a599
Zein Tonconi <zeintonconi@gmail.com>
2023-04-04 21:59:24 -0400

    Fix blancks paths

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,11 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent
+      },
+      {
+        path: "",
+        redirectTo: "login",
+        pathMatch: 'full'
       }
     ]
   }
--- src/app/todo-app/todo-app-routing.module.ts
+++ src/app/todo-app/todo-app-routing.module.ts
@@ -24,6 +24,11 @@ const routes: Routes = [
       {
         path: 'tag',
         component: TagComponent
+      },
+      {
+        path: '',
+        redirectTo: "home",
+        pathMatch: "full"
       }
     ]
   }
