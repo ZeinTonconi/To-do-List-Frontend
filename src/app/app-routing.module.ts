9f4e1e83317dd11231b527152eb940da542a57ce
Zein Tonconi <zeintonconi@gmail.com>
2023-04-04 21:51:56 -0400

    Add Page Not Found Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   
@@ -14,6 +15,10 @@ const routes: Routes = [
     {
         path: 'todo',
         loadChildren: () => import('./todo-app/todo-app.module').then(m => m.TodoAppModule)
+    },
+    {
+        path: "**",
+        component: PageNotFoundComponent
     }
 
 ];
