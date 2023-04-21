7f7916adb19cbc41442130e5db3b08715bda16bc
Zein Tonconi <zeintonconi@gmail.com>
2023-04-21 19:13:39 -0400

    Add header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,19 +3,21 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
+    MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule
   ]
 })
 export class MaterialModule { }
