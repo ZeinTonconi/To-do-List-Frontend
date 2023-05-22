f96f42edcefbfadd6e3b98ff4666d554d1100ad3
Zein Tonconi <zeintonconi@gmail.com>
2023-05-22 11:54:25 -0400

    Update Category with the API

--- src/app/todo-app/category/category.component.ts
+++ src/app/todo-app/category/category.component.ts
@@ -3,6 +3,7 @@ import { MatTable } from '@angular/material/table';
 import { EditTagModalComponent } from '../modals/edit-tag-modal/edit-tag-modal.component';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../interfaces/category.interface';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-category',
@@ -17,7 +18,8 @@ export class CategoryComponent {
   categoryData:Category[]=[];
   displayedColumns:string[]=["nro","category","action"]
 
-  constructor(private categoryService: CategoryService){
+  constructor(private categoryService: CategoryService,
+              private editCategoryModal: MatDialog){
 
     categoryService.getCategories().subscribe( (result) => {
       this.categoryData=result.categories;
@@ -25,23 +27,34 @@ export class CategoryComponent {
     });
   }
 
-  delete(tagPosition:number){
-    // this.categoryData.splice(tagPosition,1);
-    // this.table.renderRows();
+  delete(categoryPosition:number){
+    const category = this.categoryData[categoryPosition];
+    this.categoryService.deleteCategory(category.id).subscribe( (response) => {
+      this.categoryData.splice(categoryPosition,1);
+      this.table.renderRows();
+    })
+
   }
 
-  edit(tagPosition:number){
-    // const modal=this.editTagModal.open(EditTagModalComponent,{
-    //   data:{
-    //     tag: this.categoryData[tagPosition]
-    //   }
-    // })
-    // modal.afterClosed().subscribe( (result:string) => {
-    //   if(result){
-    //     // this.categoryData[tagPosition]=result;
-    //     // this.table.renderRows();
-    //   }
-    // })
+  edit(categoryPosition:number){
+    const modal=this.editCategoryModal.open(EditTagModalComponent,{
+      data:{
+        tag: this.categoryData[categoryPosition].categoryName
+      }
+    })
+
+    const category = this.categoryData[categoryPosition];
+
+    modal.afterClosed().subscribe( (result:string) => {
+      if(result){
+
+        this.categoryService.putCategory(result,category.id).subscribe( (response) => {
+          this.categoryData[categoryPosition].categoryName=result;
+          this.table.renderRows();  
+        })
+
+      }
+    })
   }
 
   openCreateTag(){
